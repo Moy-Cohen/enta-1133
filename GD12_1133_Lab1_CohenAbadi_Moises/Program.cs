4371a6f39fef3f81803a595b851edfbba8b59971
Moy-Cohen <cohenmoy@gmail.com>
2024-09-06 12:14:08 -0700

    Exploring Visual Studio

---
+++ GD12_1133_Lab1_CohenAbadi_Moises/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_CohenAbadi_Moises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Moy Cohen ENTA 1133");
+        }
+    }
+}
