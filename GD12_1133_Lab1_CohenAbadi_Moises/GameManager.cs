1ae5b3d7118b57b6b5a3749972e494d7412d20e4
Moy-Cohen <cohenmoy@gmail.com>
2024-09-13 12:55:56 -0700

    Lesson 2
    "Program Along" for the lesson on week 2

---
+++ GD12_1133_Lab1_CohenAbadi_Moises/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD12_1133_Lab1_CohenAbadi_Moises
+{
+    internal class GameManager
+    {
+        private int score = 0;
+
+        private void OnPointAcquired()
+        {
+            score++;
+            string gainedMessage = "You gained a point!";
+            Console.WriteLine(gainedMessage);
+        }
+
+        public void PlayGame()
+        {
+            Console.WriteLine("Welcome to Die vs Die!");
+            OnPointAcquired();
+        }
+
+       
+
+    }
+}
